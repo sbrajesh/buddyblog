f8637a7bd25ab52e607b82ac5231965b3e2a1908
Brajesh Singh <brajesh@cosmiccoders.com>
2015-04-11 14:19:45 +0530

    Clan BuddyBLog Actions

--- buddyblog-actions.php
+++ buddyblog-actions.php
@@ -1,11 +1,17 @@
 <?php
-class BuddyBlogActions{
-    
+/**
+ * Handles various BuddyBlog Actions
+ * 
+ */
+class BuddyBlog_Actions {
+    /**
+	 *
+	 * @var BuddyBlog_Actions 
+	 */
     private static $instance;
     
     private function __construct() {
-        //do something here
-            //register forms
+        
         /**
          * Register form for new/edit resume
          */
@@ -19,8 +25,11 @@ class BuddyBlogActions{
         
         
     }
-
-    public static function get_instance(){
+	/**
+	 * Get Singleton Instance 
+	 * @return BuddyBlog_Actions
+	 */
+    public static function get_instance() {
         
         if( !isset ( self::$instance ) )
                 self::$instance = new self();
@@ -30,36 +39,38 @@ class BuddyBlogActions{
     /**
      * Create Posts screen
      */
-    function create(){
+    public function create() {
         //No Need to implement it, BP Simple FEEditor takes care of this
     }
     /**
      * Edit Posts screen
      */
-    function edit(){
+    public function edit() {
         //No Need to implement it, BP Simple FEEditor takes care of this
     }
     /**
      * delete Post screen
      */
-    function delete(){
+    public function delete() {
         
-          if( !( bp_is_buddyblog_component() && bp_is_current_action( 'delete' ) ) )
-           return;
+		if( !( bp_is_buddyblog_component() && bp_is_current_action( 'delete' ) ) )
+		 return;
           
         $post_id = bp_action_variable( 0 );
-        if( !$post_id )
+		
+        if( ! $post_id )
             return;
         
       
          
-        if( buddyblog_user_can_delete( $post_id,  get_current_user_id() ) ){
+        if( buddyblog_user_can_delete( $post_id,  get_current_user_id() ) ) {
 
             wp_delete_post( $post_id, true );
             bp_core_add_message ( __( 'Post deleted successfully' ), 'buddyblog' );
             //redirect
             wp_redirect( buddyblog_get_home_url() );//hardcoding bad
             exit( 0 );  
+			
         }else{
          
             bp_core_add_message ( __( 'You should not perform unauthorized actions', 'buddyblog' ),'error');
@@ -69,97 +80,108 @@ class BuddyBlogActions{
     /**
      * Publish Post
      */
-    function publish(){
-       if( !( bp_is_buddyblog_component() && bp_is_current_action( 'publish' ) ) )
+    public function publish() {
+		
+       if( ! ( bp_is_buddyblog_component() && bp_is_current_action( 'publish' ) ) )
            return;
            
         $id = bp_action_variable(0);
-        if( !$id )
+		
+        if( ! $id )
             return;
        
-        if( buddyblog_user_can_publish( get_current_user_id(), $id ) ){
+        if( buddyblog_user_can_publish( get_current_user_id(), $id ) ) {
+			
             wp_publish_post( $id );//change status to publish         
             bp_core_add_message( __( 'Post Published', 'buddyblog' ) );   
         }
+		
         bp_core_redirect( buddyblog_get_home_url() );
     }
     /**
      * Unpublish a post
      */
-    function unpublish(){
+    public function unpublish() {
+		
          if( !( bp_is_buddyblog_component() && bp_is_current_action( 'unpublish' ) ) )
            return;
+		 
         $id = bp_action_variable( 0 );
-        if( !$id )
+        
+		if( ! $id )
             return;
-         if( buddyblog_user_can_unpublish( get_current_user_id(), $id ) ){
-               
-             $post = get_post( $id, ARRAY_A );
-             $post['post_status'] = 'draft';
-             wp_update_post( $post );
-             //unpublish
-                bp_core_add_message( __('Post unpublished','buddyblog') );
-                
-         }
          
-         bp_core_redirect( buddyblog_get_home_url() );
+		if( buddyblog_user_can_unpublish( get_current_user_id(), $id ) ) {
+               
+			$post = get_post( $id, ARRAY_A );
+			$post['post_status'] = 'draft';
+			wp_update_post( $post );
+			//unpublish
+			bp_core_add_message( __('Post unpublished','buddyblog') );
+                
+        }
+         
+        bp_core_redirect( buddyblog_get_home_url() );
          
     }
     
     
 
-/**
- * register post form for Posting/editing
- * @return type 
- */
+	/**
+	 * register post form for Posting/editing
+	 * @return type 
+	 */
 
-function register_form(){
-    //make sure the Front end simple post plugin is active
-    if( !function_exists( 'bp_new_simple_blog_post_form' ) )
-        return;
-    
-    $post_status = 'publish';
-    $user_id = get_current_user_id();
-    
-     if( !buddyblog_user_can_post( $user_id ) )
-         $post_status = 'draft';
-        
-    
-    $settings=array(
-        'post_type'=> buddyblog_get_posttype(),
-        'post_status' => $post_status,
-        'comment_status' => 'open',
-        'show_comment_option' => true,
-        'custom_field_title' => '',//we are only using it for hidden field, so no need to show it
-        'custom_fields'=> array(
-            '_is_buddyblog_post' => array(
-                'type'    => 'hidden',
-                'label'   => '',
-                'default' => 1
-            )
-        ),      
-        'tax'=>array(
-                   'category'=> array('taxonomy'=>'category',
-                                    'view_type'=>'checkbox'
-                                ),
-                   'post_tag'=> array('taxonomy'=>'post_tag',
-                                     'view_type'=>'checkbox'
-                                )
-          
-                ),  
-        'upload_count'=> 0,
-        'has_post_thumbnail'=> 1
-        );
-    
-   //use it to add extra fields or filter the post type etc
-    
-    $settings = apply_filters( 'buddyblog_post_form_settings', $settings );
-   
-    bp_new_simple_blog_post_form( 'buddyblog-user-posts', $settings );// the blog form
-    
-  
- 
-}
+	public function register_form() {
+		
+		//make sure the Front end simple post plugin is active
+		if( ! function_exists( 'bp_new_simple_blog_post_form' ) )
+			return;
+
+		$post_status = 'publish';
+		$user_id = get_current_user_id();
+
+		 if( ! buddyblog_user_can_post( $user_id ) )
+			 $post_status = 'draft';
+
+
+		$settings = array(
+			'post_type'				=> buddyblog_get_posttype(),
+			'post_status'			=> $post_status,
+			'comment_status'		=> 'open',
+			'show_comment_option'	=> true,
+			'custom_field_title'	=> '',//we are only using it for hidden field, so no need to show it
+			'custom_fields'			=> array(
+						'_is_buddyblog_post' => array(
+							'type'    => 'hidden',
+							'label'   => '',
+							'default' => 1
+						)
+			),      
+			'tax'					=> array(
+					   'category'=> array(
+						   'taxonomy'	=>'category',
+							'view_type'=>'checkbox'
+						),
+					   'post_tag'=> array(
+						   'taxonomy'=>'post_tag',
+							'view_type'=>'checkbox'
+						)
+
+			),  
+			'upload_count'			=> 0,
+			'has_post_thumbnail'	=> 1
+			);
+
+	   //use it to add extra fields or filter the post type etc
+
+		$settings = apply_filters( 'buddyblog_post_form_settings', $settings );
+
+		bp_new_simple_blog_post_form( 'buddyblog-user-posts', $settings );// the blog form
+
+
+
+	}
 }    
-
-BuddyBlogActions::get_instance();
+//instantiate
+BuddyBlog_Actions::get_instance();
