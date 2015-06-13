1b46d38016a91fdadf62798a9f227a5729136a7b
Brajesh Singh <brajesh@cosmiccoders.com>
2015-06-14 05:24:25 +0530

    refactor BuddyBlog Core Component

--- buddyblog-loader.php
+++ buddyblog-loader.php
@@ -7,22 +7,23 @@
  */
 
 // Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) exit;
 
-class BuddyBlogCoreComponent extends BP_Component {
+class BuddyBlog_Core_Component extends BP_Component {
 
 	/**
-         * Initiate the component creation process
-         */
-	function __construct() {
+	 * Initialize component
+	 */
+	public function __construct() {
+		
 		parent::start(
 			'buddyblog',
 			__( 'BuddyBlog', 'buddyblog' ),
-                        untrailingslashit(plugin_dir_path(__FILE__))
+            untrailingslashit(plugin_dir_path(__FILE__))
 		);
                 
-                $this->includes();//load files
-                $bp->active_components[$this->id] = 1;
+		$this->includes();//load files
+		buddypress()->active_components[$this->id] = 1;
 	}
 
 	/**
@@ -30,17 +31,18 @@ class BuddyBlogCoreComponent extends BP_Component {
 	 */
 	public function includes( $includes = array() ) {
 		$includes = array(
-                        'buddyblog-templates.php',
-                       	'buddyblog-actions.php',
+			'buddyblog-templates.php',
+			'buddyblog-actions.php',
 			'buddyblog-screens.php',
                        
 			'buddyblog-functions.php',
 			'buddyblog-notifications.php',
-                        'buddyblog-hooks.php',
-                        'core/filters.php',
-                        'core/permissions.php'
+			'buddyblog-hooks.php',
+			'core/filters.php',
+			'core/permissions.php'
                        
 		);
+		
 		parent::includes( $includes );
 	}
 
@@ -49,10 +51,9 @@ class BuddyBlogCoreComponent extends BP_Component {
 	 */
         
 	public function setup_globals( $globals = array() ) {
-		global $bp;
-
+		
 		// Define a slug, if necessary
-		if ( !defined( 'BP_BUDDYBLOG_SLUG' ) )
+		if ( ! defined( 'BP_BUDDYBLOG_SLUG' ) )
 			define( 'BP_BUDDYBLOG_SLUG', $this->id );
 		
 		$globals = array(
@@ -66,9 +67,6 @@ class BuddyBlogCoreComponent extends BP_Component {
 
 		parent::setup_globals( $globals );
 
-		
-			
-		
 	}
 
 	/**
@@ -80,65 +78,61 @@ class BuddyBlogCoreComponent extends BP_Component {
 
 		// Define local variables
 		$sub_nav = array();
-                $screen = BuddyBlogScreens::get_instance();//instance of the blog screen
+		$screen = BuddyBlogScreens::get_instance();//instance of the blog screen
+
+		$total_posts = 0;
+		
+		if( bp_is_my_profile() ) {
+			$total_posts = buddyblog_get_total_posted( bp_displayed_user_id() );
+
+		}else{
+
+		   $total_posts = buddyblog_get_total_published_posts( bp_displayed_user_id() );
+
+		}
                 
-                $total_posts = 0;
-                if( bp_is_my_profile() ){
-                    $total_posts = buddyblog_get_total_posted(bp_displayed_user_id());
-                    
-                    
-                }else{
-                    
-                   $total_posts = buddyblog_get_total_published_posts(bp_displayed_user_id());
-                    
-                }
-                
-                    //
+        //
 		// Add 'Blog' to the main navigation
 		$main_nav = array(
 			'name'                => sprintf( __( 'Blog <span>%d</span>', 'buddyblog' ), $total_posts ),
 			'slug'                => $this->slug,
 			'position'            => 70,
-			'screen_function'     => array($screen,'my_posts'),
+			'screen_function'     => array( $screen, 'my_posts' ),
 			'default_subnav_slug' => BUDDYBLOG_ARCHIVE_SLUG,
 			'item_css_id'         => $this->id
 		);
                 
-                //whether to link to logged in user or displayed user
-                if(!bp_is_my_profile())
-                    $blog_link = trailingslashit( bp_displayed_user_domain() . $this->slug );
-                else
-		$blog_link = trailingslashit( bp_loggedin_user_domain() . $this->slug );
-
+		//whether to link to logged in user or displayed user
+		if( ! bp_is_my_profile() ) {
+			
+			$blog_link = trailingslashit( bp_displayed_user_domain() . $this->slug );
+		
+		}else {
+		
+			$blog_link = trailingslashit( bp_loggedin_user_domain() . $this->slug );
+		}	
 		// Add the Group Invites nav item
-                $sub_nav[] = array(
-			'name'            => bp_is_my_profile()?__( 'My Posts', 'buddyblog' ):sprintf(__( '%s\' Posts', 'buddyblog' ),  bp_get_displayed_user_fullname()),
+        $sub_nav[] = array(
+			'name'            => bp_is_my_profile() ? __( 'My Posts', 'buddyblog' ) : sprintf( __( '%s\' Posts', 'buddyblog' ),  bp_get_displayed_user_fullname() ),
 			'slug'            => BUDDYBLOG_ARCHIVE_SLUG,
 			'parent_url'      => $blog_link,
 			'parent_slug'     => $this->slug,
-			'screen_function' => array($screen,'my_posts'),
-			
+			'screen_function' => array( $screen, 'my_posts' ),
 			'position'        => 30
 		);
+		
 		$sub_nav[] = array(
 			'name'            => __( 'New Post', 'buddyblog' ),
 			'slug'            => 'edit',
 			'parent_url'      => $blog_link,
 			'parent_slug'     => $this->slug,
-			'screen_function' => array($screen,'new_post'),
+			'screen_function' => array( $screen, 'new_post' ),
 			'user_has_access' => bp_is_my_profile(),
 			'position'        => 30
 		);
-		
-                
-                
-                
-                
-                
-               
+		               
 		parent::setup_nav( $main_nav, $sub_nav );
 	
-		
 	}
 	/**
 	 * Set up the Toolbar
@@ -146,8 +140,8 @@ class BuddyBlogCoreComponent extends BP_Component {
 	 * 
 	 */
 	public function setup_admin_bar( $nav = array() ) {
-		global $bp;
-
+		
+		$bp = buddypress();
 		// Prevent debug notices
 		$wp_admin_nav = array();
 
@@ -158,10 +152,7 @@ class BuddyBlogCoreComponent extends BP_Component {
 			$user_domain = bp_loggedin_user_domain();
 			$blog_link = trailingslashit( $user_domain . $this->slug );
 
-			
 			$title   = __( 'My Posts',             'buddyblog' );
-			
-
 			// My Posts
 			$wp_admin_nav[] = array(
 				'parent' => $bp->my_account_menu_id,
@@ -169,9 +160,6 @@ class BuddyBlogCoreComponent extends BP_Component {
 				'title'  => $title,
 				'href'   => trailingslashit( $blog_link )
 			);
-
-			
-
 			
 		}
 
@@ -184,21 +172,23 @@ class BuddyBlogCoreComponent extends BP_Component {
 	 * @global BuddyPress $bp The one true BuddyPress instance
 	 */
 	public function setup_title() {
-		global $bp;
-
+		
+		$bp = buddypress();
+		
 		if ( bp_is_buddyblog_component() ) {
 
-			if ( bp_is_my_profile() && !bp_is_single_item() ) {
+			if ( bp_is_my_profile() && ! bp_is_single_item() ) {
 
 				$bp->bp_options_title = __( 'My Posts', 'buddyblog' );
 
-			} else if ( !bp_is_my_profile() && !bp_is_single_item() ) {
+			} elseif ( ! bp_is_my_profile() && ! bp_is_single_item() ) {
 
 				$bp->bp_options_avatar = bp_core_fetch_avatar( array(
 					'item_id' => bp_displayed_user_id(),
 					'type'    => 'thumb',
 					'alt'     => sprintf( __( 'Profile picture of %s', 'buddyblog' ), bp_get_displayed_user_fullname() )
 				) );
+				
 				$bp->bp_options_title = bp_get_displayed_user_fullname();
 
 			// We are viewing a single group, so set up the
@@ -208,11 +198,8 @@ class BuddyBlogCoreComponent extends BP_Component {
 
 		parent::setup_title();
 	}
-        
-
-
  
-}
+}//End of BuddyBlog_Core_Component
 
 
 /**
@@ -220,9 +207,8 @@ class BuddyBlogCoreComponent extends BP_Component {
  * @global type $bp 
  */
 function bp_setup_buddyblog() {
-	global $bp;
-
-	$bp->buddyblog = new BuddyBlogCoreComponent();
+	
+	buddypress()->buddyblog = new BuddyBlog_Core_Component();
 }
 add_action( 'bp_loaded', 'bp_setup_buddyblog');
 
